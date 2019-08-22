c1e36a9e417e29704d53a950438f92bcb53f40a8
stuchly <grigorij@seznam.cz>
2019-08-22 15:07:33 +0200

    denoise

--- R/denoise.R
+++ R/denoise.R
@@ -12,3 +12,13 @@ data.core<-function(DIST,perc=0.8){
     dens<-1/DIST[,K]
     return(which(dens>quantile(dens,prob=perc)))
 }
+
+denoiseFix<-function(X,IND,iter=1){
+    XX<-X
+    for (i in 1:iter){
+        for (j in 1:nrow(X)){
+            X[j,]<-colMeans(XX[IND[j,],])
+        }
+    }
+    return(X)
+}
