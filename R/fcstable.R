4d8e4ad06ec49caa414c02583f6207481d87e5eb
stuchly <grigorij@seznam.cz>
2020-02-13 20:12:39 +0100

    fcstable

--- R/fcstable.R
+++ R/fcstable.R
@@ -3,5 +3,5 @@ fcstable<-function(fcs,table_file="channels.txt"){
     res<-fcs@parameters@data
     res<-data.frame(res,use=0)
     write.table(res,file=table_file,col.names=TRUE,row.names=FALSE,sep="\t")
-    return(invisible(res)
+    return(invisible(res))
 }
