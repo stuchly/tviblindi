33a5b060ebb16f1e5f0becee65cfe5ee6a8fe538
stuchly <grigorij@seznam.cz>
2020-09-16 19:46:35 +0200

    upsample method core

--- R/upsample.R
+++ R/upsample.R
@@ -1,9 +1,36 @@
-.upsample<-function(data,knn,inds,N){
-    out<-NULL
-    for (i in 1:length(inds)){
-        ee<-matrix(runif(N*ncol(knn)),ncol=N)
-        ee<-ee/colSums(ee)
-        out<-rbind(out,data[knn[i,],]%*%ee)
+.upsample<-function(data,knn,inds,N,concentration=0.1,labels=NULL){
+    lab<-out<-NULL
+    for (i in inds){
+        ee<-matrix(.rdirichlet(N,rep(concentration,ncol(knn))),byrow = TRUE,ncol=N)
+        out<-rbind(out,t(t(data[knn[i,],])%*%ee))
+        if (!is.null(labels)) lab<-c(lab,rep(labels[i],N))
     }
-    out
+    list(out=out,lab=lab)
+}
+
+.rdirichlet<-function (n, alpha)
+{
+    l <- length(alpha)
+    x <- matrix(rgamma(l * n, alpha), ncol = l, byrow = TRUE)
+    sm <- x %*% rep(1, l)
+    return(x/as.vector(sm))
+}
+
+
+upsample<-function(x,N=5,K=30,labname="default",inds=NULL,concentration=0.1){
+    if(is.null(inds)) inds<-1:nrow(x$data)
+    if(is.character(inds)){
+        out<-NULL
+        for (j in unique(inds)){
+            out<-c(out,which(x$labels[[labname]]==j))
+        }
+        inds<-out
+        rm(out)
+    }
+    K<-min(K,ncol(x$KNN$INDS))
+    ups<-.upsample(x$data,x$KNN$INDS[,1:K]+1,inds,N,concentration,as.character(x$labels[[labname]]))
+    labels<-c(as.character(x$labels[[labname]]),ups$lab)
+    data<-rbind(x$data,ups$out)
+    tv<-tviblindi(data=data,labels=labels)
+    return(invisible(tv))
 }
