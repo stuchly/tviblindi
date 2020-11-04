f969c18abe4c6e9c835442acb384bd52b0722f88
stuchly <grigorij@seznam.cz>
2020-11-04 18:33:47 +0100

    tv1->tv in shiny

--- R/shiny_server.R
+++ R/shiny_server.R
@@ -769,7 +769,7 @@ shiny_server <- function(input, output, session) {
         ps       <- as.numeric(as.factor(tv$pseudotime[[react$pathmodel_name]]$res))
         labels_out<-matrix(NA,ncol=length(tv$labels),nrow=length(tv$labels[[1]]))
         colnames(labels_out)<-paste("label",names(tv$labels),sep="_")
-        for (i in 1:length(tv$labels)) labels_out[,i]<-as.numeric(tv1$labels[[i]])
+        for (i in 1:length(tv$labels)) labels_out[,i]<-as.numeric(tv$labels[[i]])
       } else {
         layout_X <- layout_Y <- rep(-100, nrow(input_ff))
         layout_X[event_sel] <- layout.df[[react$layout_name]]$X * 100
@@ -778,7 +778,7 @@ shiny_server <- function(input, output, session) {
         ps[event_sel] <- as.numeric(as.factor(tv$pseudotime[[react$pathmodel_name]]$res))
         labels_out<-matrix(-100,ncol=length(tv$labels),nrow=nrow(input_ff))
         colnames(labels_out)<-paste("label",names(tv$labels),sep="_")
-        for (i in 1:length(tv$labels)) labels_out[event_sel,i]<-tv1$labels[[i]]
+        for (i in 1:length(tv$labels)) labels_out[event_sel,i]<-tv$labels[[i]]
       }
       react$output_ff <- fcs.add_col(
         fcs.add_col(
@@ -844,7 +844,7 @@ shiny_server <- function(input, output, session) {
         ps       <- as.numeric(as.factor(tv$pseudotime[[react$pathmodel_name]]$res))
         labels_out<-matrix(NA,ncol=length(tv$labels),nrow=length(tv$labels[[1]]))
         colnames(labels_out)<-paste("label",names(tv$labels),sep="_")
-        for (i in 1:length(tv$labels)) labels_out[,i]<-as.numeric(tv1$labels[[i]])
+        for (i in 1:length(tv$labels)) labels_out[,i]<-as.numeric(tv$labels[[i]])
 
       } else {
         layout_X <- layout_Y <- rep(-100, nrow(input_ff))
@@ -854,7 +854,7 @@ shiny_server <- function(input, output, session) {
         ps[event_sel] <- as.numeric(as.factor(tv$pseudotime[[react$pathmodel_name]]$res))
         labels_out<-matrix(-100,ncol=length(tv$labels),nrow=nrow(input_ff))
         colnames(labels_out)<-paste("label",names(tv$labels),sep="_")
-        for (i in 1:length(tv$labels)) labels_out[event_sel,i]<-tv1$labels[[i]]
+        for (i in 1:length(tv$labels)) labels_out[event_sel,i]<-tv$labels[[i]]
       }
       react$output_ff <- fcs.add_col(
         fcs.add_col(
@@ -881,7 +881,7 @@ shiny_server <- function(input, output, session) {
         ps       <- tv$pseudotime[[react$pathmodel_name]]$res
         labels_out<-matrix(NA,ncol=length(tv$labels),nrow=length(tv$labels[[1]]))
         colnames(labels_out)<-paste("label",names(tv$labels),sep="_")
-        for (i in 1:length(tv$labels)) labels_out[,i]<-as.numeric(tv1$labels[[i]])
+        for (i in 1:length(tv$labels)) labels_out[,i]<-as.numeric(tv$labels[[i]])
       } else {
         layout_X <- layout_Y <- rep(-100, nrow(input_ff))
         layout_X[event_sel] <- layout.df[[react$layout_name]]$X * 100
@@ -890,7 +890,7 @@ shiny_server <- function(input, output, session) {
         ps[event_sel] <- tv$pseudotime[[react$pathmodel_name]]$res
         labels_out<-matrix(-100,ncol=length(tv$labels),nrow=nrow(input_ff))
         colnames(labels_out)<-paste("label",names(tv$labels),sep="_")
-        for (i in 1:length(tv$labels)) labels_out[event_sel,i]<-tv1$labels[[i]]
+        for (i in 1:length(tv$labels)) labels_out[event_sel,i]<-tv$labels[[i]]
       }
       react$output_ff <- fcs.add_col(
         fcs.add_col(
