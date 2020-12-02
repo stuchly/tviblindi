0430d8afc5842dd5bc224eff0c4945afc37b5c4d
stuchly <grigorij@seznam.cz>
2020-12-02 14:30:14 +0100

    multilabel tviblindi_from_flowjo

--- R/preprocess_file.R
+++ R/preprocess_file.R
@@ -1,6 +1,6 @@
 tviblindi_from_flowjo<-function (fcsn, wsp, fcstable, eventsufix = "_G[0-9]+$", eventprefix = "/[A-Z]_",
-          origin = "^A_", normalize = FALSE, sep = "\t", ftrans = function(x) asinh(x/5),
-          comp = NULL, sampNloc = "keyword")
+                                 origin = "^A_", normalize = FALSE, sep = "\t", ftrans = function(x) asinh(x/5),
+                                 comp = NULL, sampNloc = "keyword")
 {
   require(CytoML)
   require(flowWorkspace)
@@ -35,25 +35,43 @@ tviblindi_from_flowjo<-function (fcsn, wsp, fcstable, eventsufix = "_G[0-9]+$", 
     else if (normalize == "scale")
       COO <- scale(COO)
   }
-  leafs <- NULL
-  for (ii in 1:length(PP)) if (length(grep(PP[ii], PP, fixed = TRUE)) ==
-                               1)
-    leafs <- c(leafs, ii)
-  leafs <- leafs[-1]
-  leafs <- leafs[grep(eventprefix, PP[leafs])]
-  indM <- NULL
-  for (ii in leafs) indM <- cbind(indM, as.numeric(gh_pop_get_indices_mat(gs[[1]],
-                                                                          PP[ii])))
-  colnames(indM) <- gsub(".*/", "", PP[leafs])
-  gate_ev <- colnames(indM)[apply(indM, MARGIN = 1, which.max)]
-  ss <- which(rowSums(indM) == 0)
-  gate_ev[ss] <- "ungated"
-  gate_ev_non <- gate_ev[events_sel]
-  gate_ev_non <- as.factor(gate_ev_non)
+
+  gate_ev_non<-list()[1:length(eventprefix)]
+  j<-0
+
+  for (pref in eventprefix){
+    j<-j+1
+    leafs<-grep(pref,PP)
+    indM <- NULL
+    for (ii in leafs) indM <- cbind(indM, as.numeric(gh_pop_get_indices_mat(gs[[1]],PP[ii])))
+    colnames(indM) <- gsub(".*/", "", PP[leafs])
+    gate_ev <- colnames(indM)[apply(indM, MARGIN = 1, which.max)]
+    ss <- which(rowSums(indM) == 0)
+    gate_ev[ss] <- "ungated"
+    gate_ev_non[[j]] <- gate_ev[events_sel]
+
+  }
+  ## for (ii in 1:length(PP)) if (length(grep(PP[ii], PP, fixed = TRUE)) ==
+  ##                              1)
+  ##   leafs <- c(leafs, ii)
+  ## leafs <- leafs[-1]
+  ## leafs <- leafs[grep(eventprefix, PP[leafs])]
+  ## indM <- NULL
+  ## for (ii in leafs) indM <- cbind(indM, as.numeric(gh_pop_get_indices_mat(gs[[1]],
+  ##                                                                         PP[ii])))
+  ## colnames(indM) <- gsub(".*/", "", PP[leafs])
+  ## gate_ev <- colnames(indM)[apply(indM, MARGIN = 1, which.max)]
+  ## ss <- which(rowSums(indM) == 0)
+  ## gate_ev[ss] <- "ungated"
+  ## gate_ev_non <- gate_ev[events_sel]
+  ## gate_ev_non <- as.factor(gate_ev_non)
+  names(gate_ev_non)<-paste("labels",1:length(eventprefix))
   tv1 <- tviblindi(COO, labels = gate_ev_non, fcs_path = fcsn,
                    events_sel = events_sel)
+
+
   if (!is.null(origin)) {
-    stems <- levels(gate_ev_non)[grep(origin, levels(gate_ev_non))]
+    stems <- levels(tv1$labels[[1]])[grep(origin, levels(tv1$labels[[1]]))]
     Set_origin(tv1, stems)
     if (is.null(tv1$origin))
       stop("Wrong population of origin")
